f29291b17fd8c8c9b1d540fb7e3a9f0a0aefb4b2
Leonardo Aleman <lnrd256@gmail.com>
2015-11-07 20:10:54 -0430

    subido lo basico

---
+++ js/effects.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	 $( "#show-phone" ).animate({
+    width: "100%"
+  }, 500, function() {
+   		$( "#amber" ).animate({
+		    width: "20%"
+		  }, 500, function() {});
+   		$( "#unity" ).animate({
+		    width: "20%"
+		  }, 500, function() {});
+   		$( "#r1" ).animate({
+		    width: "20%"
+		  }, 500, function() {});
+  });
+})
