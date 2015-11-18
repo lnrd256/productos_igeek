11bb0fe18452915fd7da67ecb36c76e0f0f71c8a
Leonardo Aleman <lnrd256@gmail.com>
2015-11-17 22:28:55 -0430

    agregado home y phone

--- js/effects.js
+++ js/effects.js
@@ -1,15 +1,9 @@
-$(document).ready(function(){
-	 $( "#show-phone" ).animate({
-    width: "100%"
-  }, 500, function() {
-   		$( "#amber" ).animate({
-		    width: "20%"
-		  }, 500, function() {});
-   		$( "#unity" ).animate({
-		    width: "20%"
-		  }, 500, function() {});
-   		$( "#r1" ).animate({
-		    width: "20%"
-		  }, 500, function() {});
-  });
-})
+
+$(window).resize(function(){
+	if($(window).height() <= 550){
+		$("#peke").hide()
+	}else{
+		$("#peke").show()
+	
+	}
+});
