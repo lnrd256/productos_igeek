8129406363fa3103ecf33cb4e7fe71192ecc130b
Leonardo Aleman <lnrd256@gmail.com>
2015-11-20 23:35:30 -0430

    agregado sound

--- js/effects.js
+++ js/effects.js
@@ -4,6 +4,39 @@ $(window).resize(function(){
 		$("#peke").hide()
 	}else{
 		$("#peke").show()
-	
 	}
 });
+
+$(".imagen").mouseover(function(){
+	$(this).animate({width:"100%" },150,function(){
+
+	});
+})
+$(".imagen").mouseout(function(){
+	$(this).animate({width:"90%" },150,function(){
+
+	});
+})
+
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+	var delta = 0;
+	if (event.wheelDelta) delta = event.wheelDelta / 120;
+	else if (event.detail) delta = -event.detail / 3;
+
+	handle(delta);
+	if (event.preventDefault) event.preventDefault();
+	event.returnValue = false;
+}
+
+function handle(delta) {
+	var time 	= 700;
+	var distance 	= 400;
+
+	$('html, body').stop().animate({
+		scrollTop: $(window).scrollTop() - (distance * delta)
+	}, time );
+}
