8f689af94d58f767824da59634243b37608b67d9
Leonardo Aleman <lnrd256@gmail.com>
2015-11-28 21:08:28 -0430

    subido todo

---
+++ ig-sound-ig-x05.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>iG X05 | iGeek Electronics</title>
+    <link rel="shortcut icon" href="imagenes/03/android-icon-48x48.png" />
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+
+    <!-- iGeek products CSS -->
+    <link href="css/igeekstyle.css" rel="stylesheet">
+    <link href="css/igeekmore.css" rel="stylesheet">
+
+</head>
+
+<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a  href="index.html"><img src="imagenes/03/igeek-logo1-149x35.png" alt=""></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="index.php"></a>
+                    </li>
+                   
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Section -->
+    <section id="seccion1" class="section-contexto">
+        <div class="container">
+           <img  src="imagenes/06/IGeek-Sound-X05-iG-X05-Upgraded-and-Better.png" id="img_pres" /> 
+           <br><br>
+            <h3>Upgraded and better</h3>
+            <br><br>
+            <div class="row">
+                <div class="col-md-4 col-xs-6 col-md-offset-2" id="text_derecha">
+                    <h3>The iGEEK X05 is a speaker
+                    that mix clean lines with the
+                    softness and sophistication of
+                    leather. Upgraded with even
+                    better sound specifications,
+                    user can carry it with easy and
+                    enjoy 8 hours of good music.
+                    Whithout any doubt a device
+                    with character and a own
+                    personality.
+                    </h3>
+                </div>
+                <div class="col-md-6 col-xs-6">
+                   <img  src="imagenes/06/i-GEEK-X05-X05-con-Sombra.png" id="img_seccion_1" /> 
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+   <!-- PHONE -->
+    <section id="seccion2" >
+             <img  src="imagenes/06/IGeek-X05-X05-Contexto-y-estilo-de-Vida.jpg" id="img_fondo_2" /> 
+              
+    </section>
+
+    <!-- ENERGY -->
+    <section id="seccion3">
+                 <img  src="imagenes/06/IGeek-Sound-X05-Gama-de-Colores-REFERENCIA.jpg" id="img_fondo_2" /> 
+    </section>
+
+    <!-- COMPUTERS -->
+    <section id="seccion4" class="computers-section">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-md-offset-3">
+                    <h2>SPECIFICATIONS</h2>
+                </div>
+                
+                <div class="col-md-2 col-md-offset-3">
+                    
+                    <br>
+                    <h3>DIMENSIONS</h3>
+                    <h4>186 x 54 x 76 mm</h4>
+                    <br>
+                    <h3>WEIGHT</h3>
+                     <h4>560 g</h4>
+                </div>
+                 <div class="col-md-2">
+
+                    <br>
+                    <h3>MATERIAL</h3>
+                    <h4>Case Aluminium Handle: Leather</h4>
+                    <br>
+                    <h3>CONNECTIVITY</h3>
+                     <h4>Bluetooth CSR 4.0</h4>
+                </div>
+                <div class="col-md-2">
+
+                    <br>
+                    <h3>BATTERY</h3>
+                    <h4>Rechargeable
+Li-Ion 1800mAh</h4>
+                    <br>
+                    <h3>PLAYING TIME</h3>
+                     <h4>About 8 hours</h4>
+                </div>
+            </div>
+       </div>
+    </section>
+
+    <!-- SOUND -->
+
+
+    <!-- STORE -->
+    <!--
+    <section id="store" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>STORE</h1>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+     <script src="js/effects.js"></script>
+</body>
+
+</html>
